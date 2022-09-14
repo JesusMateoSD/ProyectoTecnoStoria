16d2ba71d7d9b2b42c1c9a7685d49508639555bc
Mateo <91506679+JesusMateoSD@users.noreply.github.com>
2022-09-14 07:04:18 -0500

    V 0.1.3

---
+++ connection/pdoconfig.php
@@ -0,0 +1,5 @@
+<?php
+    $host = 'localhost';
+    $dbname = 'tecnostoriadb';
+    $username = 'root';
+    $password = '';
--- usuario/indexadm.php
+++ usuario/indexadm.php
@@ -1,21 +1,43 @@
 <!doctype html>
 <html lang="en">
-    <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS v5.2.0-beta1 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <!-- Bootstrap CSS v5.2.0-beta1 -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
-    </head>
+  </head>
 
-    <body>
+  <body>
+      <div class="container">
+        <div class="m-0 vh-100 row justify-content-center">
+          <div class="my-auto col-md-12">
+            <div class="card text-center">
+              <div class="card-header">
+                Login
+              </div>
+              <div class="card-body">
+                <form action="" method="POST">
+                  <div class="mb-3">
+                    <label>Usuario</label>
+                    <input type="text" class="form-control" name="usuario" id="usuario" aria-describedby="emailHelpId" placeholder="Usuario">
+                    <small id="emailHelpId" class="form-text text-muted">Ingrese el usuario</small>
+                  </div>
 
+                  <div class="mb-3">
+                    <label>Contrasena</label>
+                    <input type="password" class="form-control" name="contrasena" id="contrasena" placeholder="">
+                  </div>
 
-    <?php
-        include("../connection/scriptconexion.php");
-    ?>
-    </body>
+                  <button type="submit" class="btn btn-primary">Ingresar</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </body>
 </html>
